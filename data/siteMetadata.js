6b2f20a27ecd6d400c86c94566c70593eaf39390
jackraken <apple894894@gmail.com>
2022-09-13 12:19:01 +0800

    fix metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,7 +5,7 @@ const siteMetadata = {
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://https://www.luke516.blog/',
+  siteUrl: 'https://www.luke516.blog/',
   siteRepo: 'https://github.com/Luke516/next-blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
